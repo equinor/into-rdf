1dafcf022b18c9a830f1370a1b25e12376a5304a
Arne Måge <arne.mage@webstep.com>
2023-03-09 14:57:29 +0100

    fix: Fixed warning of possible null result.

--- IntoRdf/TransformationServices/TabularJsonTransformationServices/TabularJsonTransformationService.cs
+++ IntoRdf/TransformationServices/TabularJsonTransformationServices/TabularJsonTransformationService.cs
@@ -19,25 +19,27 @@ internal class TabularJsonTransformationService : ITabularJsonTransformationServ
         {
             throw new ArgumentException("Result of reading content was null.");
         }
-        DataTable dt = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
-        if (dt is not null)
+        DataTable? dt = JsonConvert.DeserializeObject<DataTable?>(json);
+        if (dt is null)
         {
-            var identityColumn = new DataColumn($"id_{subjectProperty}", typeof(Uri));
-            identityColumn.ColumnName = $"id_{subjectProperty}";
-            dt.Columns.Add(identityColumn);
-            foreach (DataRow dr in dt.Rows)
-            {
-                dr[$"id_{subjectProperty}"] = new Uri($"{transformationDetails.BaseUri}{dr[subjectProperty]}");
-            }
-            foreach (DataColumn dc in dt.Columns)
-            {
-                if (dc.ColumnName != $"id_{subjectProperty}")
-                {
-                    dc.ColumnName = new Uri($"{transformationDetails.SourcePredicateBaseUri}{dc.ColumnName}").ToString();
-                }
-            }
-            _rdfGraphService.AssertDataTable(dt, $"id_{subjectProperty}");
+            throw new ArgumentException("Attempting to parse the JSON resulted in a null value");
         }
+        var identityColumn = new DataColumn($"id_{subjectProperty}", typeof(Uri));
+        identityColumn.ColumnName = $"id_{subjectProperty}";
+        dt.Columns.Add(identityColumn);
+        foreach (DataRow dr in dt.Rows)
+        {
+            dr[$"id_{subjectProperty}"] = new Uri($"{transformationDetails.BaseUri}{dr[subjectProperty]}");
+        }
+        foreach (DataColumn dc in dt.Columns)
+        {
+            if (dc.ColumnName != $"id_{subjectProperty}")
+            {
+                dc.ColumnName = new Uri($"{transformationDetails.SourcePredicateBaseUri}{dc.ColumnName}").ToString();
+            }
+        }
+        _rdfGraphService.AssertDataTable(dt, $"id_{subjectProperty}");
+
         return GraphSupportFunctions.WriteGraphToString(_rdfGraphService.GetGraph(), outputFormat);
     }
 }
