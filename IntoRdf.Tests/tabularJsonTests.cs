9ebbe52953d44c8d4c63e04bf4416bdbda23bde4
Arne Måge <arne.mage@webstep.com>
2023-03-23 08:33:50 +0100

    chore: Fix and update uri naming in tests

--- IntoRdf.Tests/tabularJsonTests.cs
+++ IntoRdf.Tests/tabularJsonTests.cs
@@ -18,7 +18,7 @@ namespace IntoRdf.Tests
         {
             FileStream fruits = new FileStream("TestData/tabular.json", FileMode.Open);
             var identifierSegment = new TargetPathSegment("id", "id");
-            var td = new TransformationDetails(new Uri("https://IAmTabularJson.Something/"), new Uri("https://IAmTabularJson.Something/"), identifierSegment, new List<TargetPathSegment>(), RdfFormat.Turtle);
+            var td = new TransformationDetails(new Uri("https://IAmTabularJson.Something/"), new Uri("https://IAmTabularJson.Something/predicate#"), identifierSegment, new List<TargetPathSegment>(), RdfFormat.Turtle);
             var fruitTransformer = new TransformerService();
             var fruitsAsRdf = fruitTransformer.TransformTabularJson(fruits, RdfFormat.Turtle, "id", td);
             var graph = new VDS.RDF.Graph();
@@ -30,14 +30,14 @@ namespace IntoRdf.Tests
         {
             FileStream fruits = new FileStream("TestData/tabular-nestedArray.json", FileMode.Open);
             var identifierSegment = new TargetPathSegment("id", "id");
-            var td = new TransformationDetails(new Uri("https://IAmTabularJson.Something/"), new Uri("https://IAmTabularJson.Something/"), identifierSegment, new List<TargetPathSegment>(), RdfFormat.Turtle);
+            var td = new TransformationDetails(new Uri("https://IAmTabularJson.Something/"), new Uri("https://IAmTabularJson.Something/predicate#"), identifierSegment, new List<TargetPathSegment>(), RdfFormat.Turtle);
             var fruitTransformer = new TransformerService();
             var fruitsAsRdf = fruitTransformer.TransformTabularJson(fruits, RdfFormat.Turtle, "id", td);
             var graph = new VDS.RDF.Graph();
             graph.LoadFromString(fruitsAsRdf);
-            var InedibletriplesFromArray = graph.GetTriplesWithPredicate(graph.GetUriNode(new Uri("https://iamtabularjson.something/inedibleParts")));
+            var InedibletriplesFromArray = graph.GetTriplesWithPredicate(graph.GetUriNode(new Uri("https://iamtabularjson.something/predicate#inedibleParts")));
             Assert.True(InedibletriplesFromArray.Count() == 4);
-            var weightTriplesFromArray = graph.GetTriplesWithPredicate(graph.GetUriNode(new Uri("https://iamtabularjson.something/weights")));
+            var weightTriplesFromArray = graph.GetTriplesWithPredicate(graph.GetUriNode(new Uri("https://iamtabularjson.something/predicate#weights")));
             Assert.True(weightTriplesFromArray.Count() == 7);
         }
     }
