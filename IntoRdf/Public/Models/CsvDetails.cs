c5323982fd01ca56e662bd2c12b017780ad2ebac
Martin Tofteberg Ulvesæter <48928133+toftalisk@users.noreply.github.com>
2023-04-12 10:38:15 +0200

    feat: add support for csv (#42)
    Tested using IntoRdf.Tests/TestData/test.csv and cli. Modified cli to
    include inputFormat, but should probably use Spectre to create to
    separate commands as different inputFormats increases.

---
+++ IntoRdf/Public/Models/CsvDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace IntoRdf.Models
+{
+    public class CsvDetails
+    {
+        public CsvDetails(string fieldDelimiter = ",", string rowDelimiter = "\n")
+        {
+            FieldDelimiter = fieldDelimiter;
+            RowDelimiter = rowDelimiter;
+        }
+
+        public string FieldDelimiter { get; }
+        public string RowDelimiter { get; }
+    }
+}
