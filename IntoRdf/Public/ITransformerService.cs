0f9807603fe72f9b13fdc9b1ce7354c9d5fb6632
Martin Tofteberg Ulvesæter <48928133+toftalisk@users.noreply.github.com>
2023-04-14 14:00:48 +0200

    chore!: remove ontology and record from interface (#50)
    BREAKING CHANGE: Functions that where previously part of the interface
    is now removed

--- IntoRdf/Public/ITransformerService.cs
+++ IntoRdf/Public/ITransformerService.cs
@@ -6,8 +6,6 @@ namespace IntoRdf
     {
         public string TransformAml(AmlTransformationDetails transformationDetails, Stream content, RdfFormat outputFormat);
         public string TransformSpreadsheet(SpreadsheetDetails spreadsheetDetails, TransformationDetails transformationDetails, Stream content);
-        public string InferFromOntology(string ontology, string graphString, RdfFormat outputFormat);
-        public string CreateProtoRecord(Uri record, string graphString, RdfFormat outputFormat);
         public string TransformCsv(CsvDetails csvDetails, TransformationDetails transformationDetails, Stream content);
     }
 }
