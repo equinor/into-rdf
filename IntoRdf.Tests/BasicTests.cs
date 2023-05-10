54c3ae188c0e40e52a604335d4e4fa7be329be4d
Martin Tofteberg Ulvesæter <48928133+toftalisk@users.noreply.github.com>
2023-05-10 12:20:17 +0200

    fix: trailing slash bug (#57)
    * build: add test for trailing slash bug
    
    * fix: remove trailing slash from guid-iris

---
+++ IntoRdf.Tests/BasicTests.cs
@@ -0,0 +1,37 @@
+﻿using IntoRdf.Models;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace IntoRdf.Tests;
+
+public class BasicTests
+{
+    private static readonly Uri DataUri = new Uri("https://rdf.equinor.com/");
+    private static readonly Uri PredicateUri = new Uri("https://rdf.equinor.com/source/mel#");
+    private const string SheetName = "sheetName";
+
+    private readonly RdfTestUtil rdfTestUtils = new RdfTestUtil("TestData/basic.xlsx", CreateSpreadsheetDetails(), CreateTransformationDetails());
+
+
+    [Fact]
+    public void NoTrailingSlash()
+    {
+        var subjects = rdfTestUtils.GetAllSubjects();
+        Assert.All(subjects, (subject) => Assert.False(subject.EndsWith('/')));
+    }
+
+    private static SpreadsheetDetails CreateSpreadsheetDetails()
+    {
+        return new SpreadsheetDetails(SheetName, 1, 2, 1);
+    }
+
+    private static TransformationDetails CreateTransformationDetails()
+    {
+        return new TransformationDetails(DataUri,
+         PredicateUri,
+         null,
+         new List<TargetPathSegment>()
+            , RdfFormat.Turtle);
+    }
+}
